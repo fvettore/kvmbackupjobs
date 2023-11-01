6618c032716fe7d7e397f06de08e96ecb9582b69
Fabrizio Vettore <fabrizio@vettore.org>
2023-11-01 13:35:13 +0100

    Update createdb.sql

--- SQL/createdb.sql
+++ SQL/createdb.sql
@@ -40,3 +40,38 @@ CREATE TABLE `vms` (
   `last_seen` datetime DEFAULT NULL,
   PRIMARY KEY (`idvms`)
 ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+CREATE TABLE `backup_jobs` (
+  `idbackup_jobs` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) DEFAULT NULL,
+  `max_inc` int(11) DEFAULT 0 COMMENT 'Number of incremental backups before a new FULL',
+  `inc_nr` int(11) DEFAULT 0,
+  `full_nr` int(11) DEFAULT 0,
+  `enabled` int(11) DEFAULT 1,
+  `lastrun` datetime DEFAULT NULL,
+  `lastcompletion` datetime DEFAULT NULL,
+  `path` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`idbackup_jobs`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci ;
+CREATE 
+    ALGORITHM = UNDEFINED 
+    DEFINER = `root`@`localhost` 
+    SQL SECURITY DEFINER
+VIEW `jobs_vms` AS
+    SELECT 
+        `v`.`vm` AS `vm`,
+        `n`.`node` AS `node`,
+        `j`.`name` AS `job_name`,
+        `j`.`max_inc` AS `max_inc`,
+        `j`.`inc_nr` AS `inc_nr`,
+        `j`.`full_nr` AS `full_nr`,
+        `j`.`enabled` AS `enabled`,
+        `j`.`lastrun` AS `lastrun`,
+        `j`.`lastcompletion` AS `lastcompletion`,
+        `j`.`path` AS `path`
+    FROM
+        (((`backup_jobs` `j`
+        JOIN `backup_vms` `bv` ON (`bv`.`idbackup_jobs` = `j`.`idbackup_jobs`))
+        JOIN `vms` `v` ON (`bv`.`idvms` = `v`.`idvms`))
+        JOIN `nodes` `n` ON (`n`.`idnodes` = `v`.`idnodes`))
+    ORDER BY `j`.`name` , `v`.`vm`;
+
