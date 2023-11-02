5a707f95a4617b07655fce52d8a5d90d72995545
Fabrizio Vettore <fabrizio@vettore.org>
2023-11-02 08:29:06 +0100

    Update kvmbackupjobs.php
    Bugfix

--- kvmbackupjobs.php
+++ kvmbackupjobs.php
@@ -1,22 +1,23 @@
 <?php
+
 /**************************************************************************
-*	kvmbackupjobs
-*	© 2023 by Fabrizio Vettore - fabrizio(at)vettore.org
-*	V 0.1
-*    This program is free software: you can redistribute it and/or modify
-*    it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation, either version 3 of the License, or
-*    (at your option) any later version.
-*
-*    This program is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU General Public License for more details.
-*
-*    You should have received a copy of the GNU General Public License
-*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*
-**************************************************************************/
+ *	kvmbackupjobs
+ *	© 2023 by Fabrizio Vettore - fabrizio(at)vettore.org
+ *	V 0.1
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **************************************************************************/
 //Update all VMs status
 require_once __DIR__ . "/getstatus.php";
 
@@ -60,27 +61,27 @@ while ($l = $r->fetch_array()) {
                 nodes n ON n.idnodes = v.idnodes
                 where idbackup_jobs=$job_id");
         //number of objects to be backed-up        
-        $numvms=$r1->num_rows;
+        $numvms = $r1->num_rows;
         while ($l1 = $r1->fetch_array()) {
             list($vms, $node) = $l1;
 
             $backupdir = "$job_path/$job_name/$vms";
             echo "performing $backuptype in $backupdir for $vms on node $node\n";
-            $vmbackuptype=$backuptype;
+            $vmbackuptype = $backuptype;
             if ($backuptype === 'inc') {
                 //check if preexisting FULL
                 echo "looking for existance of FULL in $backupdir/$indir/*.full.data \n";
-                $cmd="ls $backupdir/$indir/*.full.data";
-                passthru($cmd, $result_code);                
-                if ($result_code!=0) {
+                $cmd = "ls $backupdir/$indir/*.full.data";
+                passthru($cmd, $result_code);
+                if ($result_code != 0) {
                     echo "Not previous FULL present, performing FULL instead of INC\n";
                     $vmbackuptype = 'full';
-                } 
+                }
             }
 
             $vmbackupstarted = date("Y-m-d H:i:s");
             //use different NBD port for each JOB on order to avoid conflicts between jobs
-            $nbdport=$job_id+10809;
+            $nbdport = $job_id + 10809;
             $cmd = "/usr/bin/virtnbdbackup -P $nbdport -l $vmbackuptype -U qemu+ssh://root@$node/system -d $vms -o  $backupdir/$indir/  --checkpointdir $backupdir/checkpoints";
             echo "$cmd\n";
             ob_start();
@@ -102,7 +103,7 @@ while ($l = $r->fetch_array()) {
             }
             //update array with vms backup data for notification
             $bkres[] = array('vm' => $vms, 'start' => $vmbackupstarted, 'end' => $vmbackupended, 'result' => $result, 'error' => $bkerror, 'type' => $vmbackuptype);
-            $err=$db->real_escape_string($bkerror);
+            $err = $db->real_escape_string($bkerror);
             $db->query("insert into backup_log set vm=\"$vms\", job=\"$job_name\", 
                  timestart='$vmbackupstarted',timeend='$vmbackupended',result='$result',type='$vmbackuptype',error='$err'");
         }
@@ -113,28 +114,29 @@ while ($l = $r->fetch_array()) {
         //backup completed
         if ($BACKUPFAIL) {
             $color = "red";
-            $report="FAIL";
+            $report = "FAIL";
         } else {
             $color = "green";
-            $report="SUCCESS";
+            $report = "SUCCESS";
         }
-        $thstyle="style=\"padding-top: 12px;padding-bottom: 12px;text-align: left;background-color: $color;color: white; border: 1px solid #ddd;padding: 8px;\"";
-        $tdstyle="style=\"border: 1px solid #ddd; padding: 8px;\"";
-        $tablestyle="style=\"font-family: Arial, Helvetica, sans-serif; border-collapse: collapse;width: 100%;\"";
+        $thstyle = "style=\"padding-top: 12px;padding-bottom: 12px;text-align: left;background-color: $color;color: white; border: 1px solid #ddd;padding: 8px;\"";
+        $tdstyle = "style=\"border: 1px solid #ddd; padding: 8px;\"";
+        $tablestyle = "style=\"font-family: Arial, Helvetica, sans-serif; border-collapse: collapse;width: 100%;\"";
         $message = "
         <table $tablestyle>
             <tr>
                 <th $thstyle>VM</th><th $thstyle>start</th><th $thstyle>end</th><th $thstyle>result</th><th $thstyle>type</th><th $thstyle>error</th>
             </tr>
         ";
-        if($vmres['result']=='SUCCESS')$rescolor='green';else $rescolor='red';
         foreach ($bkres as $vmres) {
+            if ($vmres['result'] == 'SUCCESS') $rescolor = 'green';
+            else $rescolor = 'red';
             $message .= "
             <tr>
                 <td $tdstyle>" . $vmres['vm'] . "</td>
                 <td $tdstyle> " . $vmres['start'] . "</td>
                 <td $tdstyle>" . $vmres['end'] . "</td>
-                <td $tdstyle><span style=\"color:$rescolor;\">". $vmres['result'] . "</span></td>            
+                <td $tdstyle><span style=\"color:$rescolor;\">" . $vmres['result'] . "</span></td>            
                 <td $tdstyle>" . $vmres['type'] . "</td>
                 <td $tdstyle>" . $vmres['error'] . "</td>
             </tr>    
@@ -142,6 +144,7 @@ while ($l = $r->fetch_array()) {
         }
         $message .= "</table>";
         emailnotify("[$report] Backup $job_name ($numvms objects)", $message);
+        unset($bkres);
     } else { //backup aboreted with error
         $message = "<h3>$ERROR</h3>";
         emailnotify("[$report] Backup $job_name ($numvms objects)", $message);
